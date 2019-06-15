2f854303f8377361ddb6bbf4f8d0c6a2d9f32914
ganesh karewad <gskaread@gmail.com>
2019-06-15 18:17:46 +0530

    Check duplicate character
    - Adds code to check duplicate character in string
    - Need to optimize code

--- problem-solution/duplicate.js
+++ problem-solution/duplicate.js
@@ -1,1 +1,14 @@
-//todo
+/**
+ * Check if the string has duplicate characters in it
+ */
+function hasUnique(str) {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (str[i] == str[j]) return false;
+        }
+    }
+    return true;
+}
+
+console.log(hasUnique("abcd"));
+console.log(hasUnique("aqwertyuiopnmxcksjdfglaa"));
