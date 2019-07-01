04819a482242bb021d7356b354c2ff88623b316a
ganesh karewad <gskaread@gmail.com>
2019-07-01 07:49:14 +0530

    Fibonacci seq

--- problem-solution/fibonacci.js
+++ problem-solution/fibonacci.js
@@ -1,1 +1,22 @@
 //todo
+
+function fib(num) {
+    if (num == 0) {
+        return 0;
+    } if (num == 1) {
+        return 1;
+    } else {
+        return fib(num - 1) + fib(num - 2);
+    }
+}
+
+function printFib(num) {
+    let ans = [];
+    for (let i = 0; i < num; i++) {
+        ans.push(fib(i));
+    }
+    console.log(ans)
+}
+
+printFib(6);
+printFib(10);
